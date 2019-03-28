e15e7f1b8f69423c73729c67bd6f3d9ecbac4599
prodes13 <ciobotaru.florin@gmail.com>
2019-03-28 18:51:21 +0200

    added eslint, first particles

--- src/PreloadScene.js
+++ src/PreloadScene.js
@@ -1,4 +1,4 @@
-import { Scene } from 'phaser'
+import Phaser, { Scene } from 'phaser'
 
 class PreloadScene extends Scene {
   constructor() {
@@ -6,11 +6,18 @@ class PreloadScene extends Scene {
   }
   preload() {
     this.load.image('logo', 'assets/logo.png');
+    this.load.image('particle', 'assets/bomb.png');
   }
   create() {
     this.add.image(400, 300, 'logo')
     this.add.text(400, 500, 'Click to start!', { fontSize: '64px', fill: 'green' }).setOrigin(.5)
     this.input.on('pointerdown', () => this.scene.start('game'));
+
+    const p = this.add.particles('particle');
+    const e = p.createEmitter();
+    e.setPosition(400, 500);
+    e.setSpeed(200);
+    e.setBlendMode(Phaser.BlendModes.ADD);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Game } from 'phaser'
 import { config } from './config'
 
-var game = new Game(config);
+new Game(config);
 
 
 // https://www.youtube.com/watch?v=-BSz_FeWfkY&list=PLoN_ejT35AEhY4icjiEJ5t2qdunwmQj1R&index=18
